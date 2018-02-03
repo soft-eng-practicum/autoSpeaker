f3dee24913c56c88ea00e75669add0ce866fb1d6
GianlucaParilli <gluka47@hotmail.com>
2018-02-02 19:07:58 -0500

    initial android workspace

---
+++ app/src/main/java/gluka/autospeakerphone/MainActivity.java
@@ -0,0 +1,16 @@
+package gluka.autospeakerphone;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        setTheme(R.style.AppTheme);
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+}
