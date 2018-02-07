f49b665bedfdc4dabfcef99977545cd9570e7776
briantorok10 <btorok1@ggc.edu>
2018-02-07 11:36:07 -0500

    Tested on my machine

--- app/src/main/java/gluka/autospeakerphone/MainActivity.java
+++ app/src/main/java/gluka/autospeakerphone/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         //testing git
         //testing VCS - Henry
+        //tested - brian
     }
 
 
