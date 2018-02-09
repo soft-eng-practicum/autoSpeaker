fec01d17fd80b9dce03ff5e2684718c47ec34555
GianlucaParilli <gluka47@hotmail.com>
2018-02-08 23:57:29 -0500

    created fav list activity, service and switch

--- app/src/main/java/gluka/autospeakerphone/MainActivity.java
+++ app/src/main/java/gluka/autospeakerphone/MainActivity.java
@@ -1,19 +1,54 @@
 package gluka.autospeakerphone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.telephony.TelephonyManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CompoundButton;
+import android.widget.Switch;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+Intent serviceIntent;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //testing git
-        //testing VCS - Henry
-        //tested - brian
+        final Switch tb = (Switch)findViewById(R.id.switch1);
+        final Button favList = (Button)findViewById(R.id.favList);
+
+        // call the service with all the telephony stuff
+        startService(new Intent(this,TelephonyService.class));
+
+        //testing without sim card in my phone -- it works
+        if(TelephonyManager.SIM_STATE_ABSENT ==1 )
+           Toast.makeText(getApplicationContext(),"no sim",Toast.LENGTH_SHORT).show();
+
+        favList.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent favorite = new Intent(getApplicationContext(), FavoriteList.class);
+                startActivity(favorite);
+            }
+        });
+        //messing with the switch
+        tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(isChecked){
+                    Toast.makeText(getApplicationContext(),"On",Toast.LENGTH_SHORT).show();
+                }
+                else if(isChecked==false){
+                    Toast.makeText(getApplicationContext(),"OFF",Toast.LENGTH_SHORT).show();
+
+                }
+            }
+        });
     }
+
 
 
 }
--- app/src/main/java/gluka/autospeakerphone/TelephonyService.java
+++ app/src/main/java/gluka/autospeakerphone/TelephonyService.java
@@ -1,10 +1,70 @@
 package gluka.autospeakerphone;
 
 import android.app.Service;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.telephony.PhoneStateListener;
+import android.telephony.TelephonyManager;
+import android.widget.Toast;
+
+import org.w3c.dom.Text;
 
 /**
  * Created by Gluka on 2/8/18.
  */
 
 public class TelephonyService extends Service {
+
+    TelephonyManager telephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
+    final Text text = null;
+    Intent i = new Intent(this,MainActivity.class);
+
+
+    // Create a new PhoneStateListener
+    PhoneStateListener listener = new PhoneStateListener() {
+
+        @Override
+        public void onCallStateChanged(int state, String incomingNumber) {
+            String stateString = "N/A";
+            switch (state) {
+                case TelephonyManager.CALL_STATE_IDLE:
+                    stateString = "Idle";
+                    Toast.makeText(getApplicationContext(),"idle",Toast.LENGTH_SHORT).show();
+
+                    break;
+                case TelephonyManager.CALL_STATE_OFFHOOK:
+                    stateString = "Off Hook";
+                    Toast.makeText(getApplicationContext(),"OFF",Toast.LENGTH_SHORT).show();
+                    break;
+                case TelephonyManager.CALL_STATE_RINGING:
+                    stateString = "Ringing";
+                    Toast.makeText(getApplicationContext(),"Ring",Toast.LENGTH_SHORT).show();
+                    break;
+
+
+            }
+            if(TelephonyManager.NETWORK_TYPE_UNKNOWN==0){
+                Toast.makeText(getApplicationContext(),"wrong",Toast.LENGTH_SHORT).show();
+
+            }
+            if(TelephonyManager.SIM_STATE_ABSENT==1){
+                Toast.makeText(getApplicationContext(),"no sim",Toast.LENGTH_SHORT).show();
+
+            }
+
+        }
+
+    };
+
+
+    @Override
+    public IBinder onBind(Intent intent) {
+        intent = new Intent(this, MainActivity.class);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
+
+        return null;
+    }
+
 }
