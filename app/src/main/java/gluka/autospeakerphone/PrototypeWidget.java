f40d28c81ad56ee5fcecd641a33f9895399c7568
Henry Huynh <henryhuynh404@gmail.com>
2018-02-09 01:26:57 -0500

    Fixed Widget "Not Loading Error"

--- app/src/main/java/gluka/autospeakerphone/PrototypeWidget.java
+++ app/src/main/java/gluka/autospeakerphone/PrototypeWidget.java
@@ -3,6 +3,8 @@ package gluka.autospeakerphone;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
 import android.content.Context;
+import android.view.View;
+import android.widget.ImageView;
 import android.widget.RemoteViews;
 
 /**
@@ -10,24 +12,14 @@ import android.widget.RemoteViews;
  */
 public class PrototypeWidget extends AppWidgetProvider {
 
-
+    ImageView speaker;
 
     static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
                                 int appWidgetId) {
-
-        CharSequence widgetText = context.getString(R.string.appwidget_text);
-        // Construct the RemoteViews object
-        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.prototype_widget);
-
-
-        views.setTextViewText(R.id.speakerView, widgetText);
-
-        // Instruct the widget manager to update the widget
-        appWidgetManager.updateAppWidget(appWidgetId, views);
     }
 
     @Override
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+    public void onUpdate(final Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         // There may be multiple widgets active, so update all of them
         for (int appWidgetId : appWidgetIds) {
             updateAppWidget(context, appWidgetManager, appWidgetId);
@@ -37,11 +29,13 @@ public class PrototypeWidget extends AppWidgetProvider {
     @Override
     public void onEnabled(Context context) {
         // Enter relevant functionality for when the first widget is created
+
     }
 
     @Override
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
+
 }
 
