fe258ef559f5d6cfd3943fbfcb9b82ff5ad46f59
Sinna Uy <suy@ggc.edu>
2018-02-12 15:35:47 -0500

    UPDATED AndroidManifest & MainActivity: Switch is able to toggle speakerphone on/off

--- app/src/main/java/gluka/autospeakerphone/MainActivity.java
+++ app/src/main/java/gluka/autospeakerphone/MainActivity.java
@@ -1,5 +1,6 @@
 package gluka.autospeakerphone;
 
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -9,9 +10,12 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
+import android.media.AudioManager;
 
 public class MainActivity extends AppCompatActivity {
-Intent serviceIntent;
+    Intent serviceIntent;
+    AudioManager audioManager;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppTheme);
@@ -19,6 +23,9 @@ Intent serviceIntent;
         setContentView(R.layout.activity_main);
         final Switch tb = (Switch)findViewById(R.id.switch1);
         final Button favList = (Button)findViewById(R.id.favList);
+
+        //Required to set Speakerphone On/Off
+        audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
 
         // call the service with all the telephony stuff
         startService(new Intent(this,TelephonyService.class));
@@ -38,12 +45,16 @@ Intent serviceIntent;
         tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
                 if(isChecked){
                     Toast.makeText(getApplicationContext(),"On",Toast.LENGTH_SHORT).show();
+                    //Set Speakerphone On
+                    audioManager.setSpeakerphoneOn(isChecked);
                 }
                 else if(isChecked==false){
                     Toast.makeText(getApplicationContext(),"OFF",Toast.LENGTH_SHORT).show();
-
+                    //Set Speakerphone Off
+                    audioManager.setSpeakerphoneOn(isChecked);
                 }
             }
         });
