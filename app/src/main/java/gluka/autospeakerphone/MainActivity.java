fae25c398dc8f682f359d171f08e636d62e08ce2
Sinna Uy <suy@ggc.edu>
2018-02-12 23:21:44 -0500

    UPDATED AndroidManifest, MainActivity, & PrototypeWidget: AppWidget toggle off/on in progress

--- app/src/main/java/gluka/autospeakerphone/MainActivity.java
+++ app/src/main/java/gluka/autospeakerphone/MainActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CompoundButton;
@@ -15,6 +16,7 @@ import android.media.AudioManager;
 public class MainActivity extends AppCompatActivity {
     Intent serviceIntent;
     AudioManager audioManager;
+    boolean isSpeakerphoneOn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,21 +47,20 @@ public class MainActivity extends AppCompatActivity {
         tb.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-
+                isSpeakerphoneOn = isChecked;
                 if(isChecked){
                     Toast.makeText(getApplicationContext(),"On",Toast.LENGTH_SHORT).show();
                     //Set Speakerphone On
                     audioManager.setSpeakerphoneOn(isChecked);
+                    Log.d("Switch in progress","true");
                 }
                 else if(isChecked==false){
                     Toast.makeText(getApplicationContext(),"OFF",Toast.LENGTH_SHORT).show();
                     //Set Speakerphone Off
                     audioManager.setSpeakerphoneOn(isChecked);
+                    Log.d("Switch in progress","false");
                 }
             }
         });
     }
-
-
-
 }
--- app/src/main/java/gluka/autospeakerphone/PrototypeWidget.java
+++ app/src/main/java/gluka/autospeakerphone/PrototypeWidget.java
@@ -1,9 +1,14 @@
 package gluka.autospeakerphone;
 
+import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
+import android.content.ComponentName;
 import android.content.Context;
-import android.view.View;
+import android.content.Intent;
+import android.media.AudioManager;
+import android.util.Log;
+import android.widget.CompoundButton;
 import android.widget.ImageView;
 import android.widget.RemoteViews;
 
@@ -13,17 +18,48 @@ import android.widget.RemoteViews;
 public class PrototypeWidget extends AppWidgetProvider {
 
     ImageView speaker;
-
-    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
-                                int appWidgetId) {
-    }
+    MainActivity mainActivity = new MainActivity();
 
     @Override
     public void onUpdate(final Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+
         // There may be multiple widgets active, so update all of them
-        for (int appWidgetId : appWidgetIds) {
-            updateAppWidget(context, appWidgetManager, appWidgetId);
+        /**
+         for (int appWidgetId : appWidgetIds) {
+         updateAppWidget(context, appWidgetManager, appWidgetId);
+         }
+         */
+
+        final int N = appWidgetIds.length;
+
+        // Perform this loop procedure for each App Widget that belongs to this provider
+        for (int i=0; i<N; i++) {
+            int appWidgetId = appWidgetIds[i];
+
+            // Create an Intent to launch ExampleActivity
+            Intent intent = new Intent(context, PrototypeWidget.class);
+            PendingIntent pendingIntent = PendingIntent.getActivity(context, 1, intent, 0);
+
+            // Get the layout for the App Widget and attach an on-click listener
+            // to the button
+            RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.prototype_widget);
+            views.setOnClickPendingIntent(R.id.imageView, pendingIntent);
+
+            if(mainActivity.isSpeakerphoneOn) {
+                //mainActivity.audioManager.setSpeakerphoneOn(true);
+                Log.d("AppWidget in progress","true");
+            }
+            else if(mainActivity.isSpeakerphoneOn == false) {
+                //mainActivity.audioManager.setSpeakerphoneOn(false);
+                Log.d("AppWidget in progress","false");
+            }
+
+            // Tell the AppWidgetManager to perform an update on the current app widget
+            appWidgetManager.updateAppWidget(appWidgetId, views);
         }
+    }
+
+    static void updateAppWidgets(Context context) {
     }
 
     @Override
@@ -36,6 +72,5 @@ public class PrototypeWidget extends AppWidgetProvider {
     public void onDisabled(Context context) {
         // Enter relevant functionality for when the last widget is disabled
     }
-
 }
 
