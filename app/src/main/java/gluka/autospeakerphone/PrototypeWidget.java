f5e0dc1f1faa18cd6c2016aa15357cb20122c932
Sinna Uy <suy@ggc.edu>
2018-02-18 19:39:11 -0500

    UPDATED PrototypeWidget & prototype_widget.xml: AppWidget is able to change imageButton on/off with toggle_image.xml

--- app/src/main/java/gluka/autospeakerphone/PrototypeWidget.java
+++ app/src/main/java/gluka/autospeakerphone/PrototypeWidget.java
@@ -1,7 +1,5 @@
 package gluka.autospeakerphone;
 
-import android.app.Activity;
-import android.app.IntentService;
 import android.app.PendingIntent;
 import android.appwidget.AppWidgetManager;
 import android.appwidget.AppWidgetProvider;
@@ -11,7 +9,6 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.util.Log;
-import android.widget.ImageView;
 import android.widget.RemoteViews;
 
 import static android.content.ContentValues.TAG;
@@ -21,14 +18,13 @@ import static android.content.ContentValues.TAG;
  */
 public class PrototypeWidget extends AppWidgetProvider
 {
-    public static final String IS_SPEAKERPHONEON="MyPref";
     AudioManager audioManager;
     boolean isSpeakerphoneOn;
 
     @Override
     public void onReceive(Context context, Intent intent)
     {
-        switchToggle(context, intent);
+        appSwitch(context, intent);
         super.onReceive(context, intent);
     }
 
@@ -45,31 +41,31 @@ public class PrototypeWidget extends AppWidgetProvider
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
             RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.prototype_widget);
-            remoteViews.setOnClickPendingIntent(R.id.imageView, pendingIntent);
+            remoteViews.setOnClickPendingIntent(R.id.imageButton, pendingIntent);
 
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
         }
     }
     // Switch method that toggles speakerphone on/off
-    private void switchToggle(Context context, Intent intent)
+    private void appSwitch(Context context, Intent intent)
     {
         SharedPreferences sharedPreferences = context.getSharedPreferences(TAG, Context.MODE_PRIVATE);
         audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        isSpeakerphoneOn = sharedPreferences.getBoolean(IS_SPEAKERPHONEON, false);
+        isSpeakerphoneOn = sharedPreferences.getBoolean("", true);
         RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.prototype_widget);
         Log.d(TAG,"Toggle State: " + isSpeakerphoneOn);
         if (isSpeakerphoneOn)
         {
             // AppWidget speaker image on
-            remoteViews.setImageViewResource(R.id.imageView, R.drawable.autospeakerlogo);
+            remoteViews.setImageViewResource(R.id.imageButton, R.drawable.autoon);
             // Set speakerphone on
             audioManager.setSpeakerphoneOn(isSpeakerphoneOn);
         }
         else
         {
             // AppWidget speaker image on
-            remoteViews.setImageViewResource(R.id.imageView, R.drawable.autospeakerlogo);
+            remoteViews.setImageViewResource(R.id.imageButton, R.drawable.autooff);
             // Set speakerphone Off
             audioManager.setSpeakerphoneOn(isSpeakerphoneOn);
         }
@@ -78,7 +74,7 @@ public class PrototypeWidget extends AppWidgetProvider
         isSpeakerphoneOn = !isSpeakerphoneOn;
         // Commit changes to SharedPreference
         SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean(IS_SPEAKERPHONEON, isSpeakerphoneOn);
+        editor.putBoolean("", isSpeakerphoneOn);
         editor.commit();
     }
 }
