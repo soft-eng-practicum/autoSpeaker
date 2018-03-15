322d9621b681ab6171d9e5016f68926e815a457f
GianlucaParilli <gluka47@hotmail.com>
2018-03-15 12:15:54 -0400

    widget is synced but with bug when widget is pressed

--- app/src/main/java/gluka/autospeakerphone/MainActivity.java
+++ app/src/main/java/gluka/autospeakerphone/MainActivity.java
@@ -1,11 +1,14 @@
 package gluka.autospeakerphone;
 
+import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.media.AudioManager;
 import android.os.Bundle;
+import android.os.IBinder;
 import android.preference.PreferenceManager;
+import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
@@ -13,16 +16,21 @@ import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.Toast;
+
 import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
     private static AudioManager audioManager;
-    private static boolean isSpeakerphoneOn;
+    private boolean isSpeakerphoneOn;
     private Context context;
     private Intent intent;
     private PhoneStateListener phoneStateListener = new PhoneStateListener();
     private Switch switch1;
     private boolean isSpeakerOn = true;
+    SharedPreferences prefs = null;
+
+
+
 
     @Override
     protected void onCreate(final Bundle savedInstanceState)
@@ -31,16 +39,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         final Button favList = (Button)findViewById(R.id.favList);
-        final SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+        prefs = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         switch1 = (Switch)findViewById(R.id.switch1);
         Log.d(TAG, "onCreate State: " + prefs.getBoolean("switchKey",true));
         //Required to set Speakerphone On/Off
         audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
 
-
         // call the service with all the telephony stuff
         startService(new Intent(this,TelephonyService.class));
-
 
         favList.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -60,7 +66,7 @@ public class MainActivity extends AppCompatActivity {
      * Set speakerphone status
      * @param isChecked
      */
-    protected static void setSpeaker(boolean isChecked)
+    public void setSpeaker(boolean isChecked)
     {
         audioManager.setSpeakerphoneOn(isChecked);
         isSpeakerphoneOn = isChecked;
@@ -70,7 +76,7 @@ public class MainActivity extends AppCompatActivity {
      * Get speakerphone status
      * @return
      */
-    protected static boolean getSpeaker()
+    protected boolean getSpeaker()
     {
         return isSpeakerphoneOn;
     }
@@ -80,36 +86,87 @@ public class MainActivity extends AppCompatActivity {
      * @param switch1
      * @param prefs
      */
-    protected void speakerphoneSwitch(Switch switch1,final SharedPreferences prefs)
+    public void speakerphoneSwitch(Switch switch1,final SharedPreferences prefs)
     {
         switch1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
-                PrototypeWidget.updateWidgets(MainActivity.this, isChecked);
+
+                WidgetUpdateService widgetService = new WidgetUpdateService();
+
+
                 if(isChecked){
                     Toast.makeText(getApplicationContext(),"On",Toast.LENGTH_SHORT).show();
                     //Set Speakerphone On
                     setSpeaker(isChecked);
+                    widgetService.widgetON();
                     //save state onto the phone hdd, not ram
-                    prefs.edit().putBoolean("switchKey", true).commit();
-                    prefs.edit().apply();
-                    isSpeakerOn = prefs.getBoolean("switchKey",true);
+
+                    prefs.edit().putBoolean("switchKey", true).apply();
+                    //isSpeakerOn = prefs.getBoolean("switchKey",true);
                     Log.d(TAG, "MainSwitch State: " + getSpeaker());
                     // Listens to phone state when switch is turn on
                     phoneStateListener.onReceive(context,intent);
+
                 }
-                else if(isChecked==false){
+                   if(isChecked==false){
                     Toast.makeText(getApplicationContext(),"OFF",Toast.LENGTH_SHORT).show();
                     //Set Speakerphone Off
                     setSpeaker(isChecked);
+                    widgetService.widgetOFF();
+
                     phoneStateListener.onReceive(context,intent);
+
+
                     //save state onto the phone hdd, not ram
-                    prefs.edit().putBoolean("switchKey", false).commit();
-                    prefs.edit().apply();
-                    isSpeakerOn = prefs.getBoolean("switchKey",false);
+                     prefs.edit().putBoolean("switchKey", false).apply();
+                    //isSpeakerOn = prefs.getBoolean("switchKey",false);
                     Log.d(TAG, "MainSwitch State: "+ getSpeaker());
                 }
             }
         });
+    }
+
+    @Override
+    protected void onPostResume() {
+        boolean t = prefs.getBoolean("switchKey",false);
+        super.onPostResume();
+
+        switch1.setChecked(t);
+        Log.d("app", "onResume: "+ t);
+
+    }
+
+
+    public class WidgetUpdateService extends Service {
+        PrototypeWidget widget = new PrototypeWidget();
+        //SharedPreferences shared = PreferenceManager.getDefaultSharedPreferences(MainActivity.this);
+        Context mainContext;
+
+        public WidgetUpdateService(){
+            mainContext = MainActivity.this.getApplicationContext();
+        }
+
+        public void widgetON(){
+            //widget.updateWidgets(mainContext);
+            Intent intent = new Intent(mainContext, PrototypeWidget.class);
+            intent.setAction("android.appwidget.action.APPWIDGET_UPDATE");
+            mainContext.sendBroadcast(intent);
+            //Log.d("app", "MainSwitch widgetON: "+ isSpeakerphoneOn);
+            //Log.d("app", "MainSwitch WIDgetOn: "+ widget.isAppWidgetOn);
+
+        }
+        public void widgetOFF(){
+            //widget.updateWidgets(mainContext);
+            Intent intent = new Intent(mainContext, PrototypeWidget.class);
+            intent.setAction("android.appwidget.action.APPWIDGET_UPDATE");
+            mainContext.sendBroadcast(intent);
+        }
+
+        @Nullable
+        @Override
+        public IBinder onBind(Intent intent) {
+            return null;
+        }
     }
 }
--- app/src/main/java/gluka/autospeakerphone/PrototypeWidget.java
+++ app/src/main/java/gluka/autospeakerphone/PrototypeWidget.java
@@ -11,135 +11,116 @@ import android.media.AudioManager;
 import android.preference.PreferenceManager;
 import android.util.Log;
 import android.widget.RemoteViews;
-import static android.content.ContentValues.TAG;
 
 /**
  * Implementation of App Widget functionality.
  */
-public class PrototypeWidget extends AppWidgetProvider
-{
+public class PrototypeWidget extends AppWidgetProvider {
     private AudioManager audioManager;
-    private SharedPreferences sharedPreferences;
-    private boolean isSpeakerphoneOn = true;
-    private static boolean isAppWidgetOn;
-    private static boolean adjustSwitch = false;
-    private static boolean intiSpeakerOn = true;
+    SharedPreferences sharedPreferences;
+    private boolean isSpeakerphoneOn;
+    private boolean isAppWidgetOn;
+    private RemoteViews remoteViews;
+
 
     @Override
-    public void onReceive(Context context, Intent intent)
-    {
-        appSwitch(context, intent);
+    public void onReceive(Context context, Intent intent) {
+        appSwitch(context);
+
         super.onReceive(context, intent);
     }
 
-    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds)
-    {
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
         super.onUpdate(context, appWidgetManager, appWidgetIds);
+
+        remoteViews = new RemoteViews(context.getPackageName(), R.layout.prototype_widget);
+
         // Perform this loop procedure for each App Widget that belongs to this provider
-        for (int widgetId : appWidgetIds)
-        {
+        for (int widgetId : appWidgetIds) {
             // Create an Intent to launch Activity
             Intent intent = new Intent(context, PrototypeWidget.class);
             PendingIntent pendingIntent = PendingIntent.getBroadcast(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
-
+            intent.putExtra(AppWidgetManager.EXTRA_APPWIDGET_ID, widgetId);
             // Get the layout for the App Widget and attach an on-click listener
             // to the button
-            RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.prototype_widget);
             remoteViews.setOnClickPendingIntent(R.id.imageButton, pendingIntent);
-
             // Tell the AppWidgetManager to perform an update on the current app widget
             appWidgetManager.updateAppWidget(widgetId, remoteViews);
+
         }
+
     }
 
     /**
      * MainActivity updates appwidget
-     * @param context
-     * @param isSpeakerOn
      */
-    protected static void updateWidgets(Context context, boolean isSpeakerOn)
-    {
-        adjustSwitch = true;
-        isAppWidgetOn = isSpeakerOn;
-        Log.d("updateWidget: ", isAppWidgetOn + "");
+    public void updateWidgets(Context context) {
+
+        //Log.d("updateWidget: ", isAppWidgetOn + "");
+        //setContextwidget(c);
         Intent intent = new Intent(context, PrototypeWidget.class);
         intent.setAction("android.appwidget.action.APPWIDGET_UPDATE");
-        intent.putExtra("appWidgetIds", AppWidgetManager.getInstance(context).getAppWidgetIds(new ComponentName(context, PrototypeWidget.class)));
         context.sendBroadcast(intent);
     }
 
-    /**
-     * Initial start up ON then generates OFF/ON
-     * @param context
-     * @return
-     */
-    protected boolean generateSwitch(Context context)
-    {
-        sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        if(intiSpeakerOn)
-        {
-            isSpeakerphoneOn = true;
-            intiSpeakerOn = false;
-            Log.d(TAG,"BEFORE AppWidget State: " + isSpeakerphoneOn);
-        }
-        else if(intiSpeakerOn == false)
-        {
-            isSpeakerphoneOn = sharedPreferences.getBoolean("", true);
-            Log.d(TAG,"AFTER AppWidget State: " + isSpeakerphoneOn);
-        }
-        return isSpeakerphoneOn;
-    }
-
-    /**
-     * MainActivity switch controller
-     */
-    private void mainSwitchControl()
-    {
-        if(isAppWidgetOn && adjustSwitch)
-        {
-            isSpeakerphoneOn = isAppWidgetOn;
-            adjustSwitch = false;
-        }
-        else if((isAppWidgetOn == false) && adjustSwitch)
-        {
-            isSpeakerphoneOn = isAppWidgetOn;
-            adjustSwitch = false;
-        }
-    }
 
     /**
      * Switch method that toggles appwidget speakerphone on/off
+     *
      * @param context
-     * @param intent
+     * @param
      */
-    protected void appSwitch(Context context, Intent intent)
-    {
+    public void appSwitch(Context context) {
+
         sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-        RemoteViews remoteViews = new RemoteViews(context.getPackageName(),R.layout.prototype_widget);
+        RemoteViews remoteViews = new RemoteViews(context.getPackageName(), R.layout.prototype_widget);
+
         audioManager = (AudioManager) context.getSystemService(Context.AUDIO_SERVICE);
-        isSpeakerphoneOn = generateSwitch(context);
-        mainSwitchControl();
-        if (isSpeakerphoneOn)
-        {
+        Log.d("app", "appSwitch: before" +isSpeakerphoneOn);
+        isSpeakerphoneOn = sharedPreferences.getBoolean("", false);//generateSwitch();
+        isAppWidgetOn = isSpeakerphoneOn;
+
+        Log.d("app", "appSwitch: after:::" +isSpeakerphoneOn);
+       // isAppWidgetOn = sharedPreferences.getBoolean("switchKey", false);//generateSwitch();
+        //isAppWidgetOn = isSpeakerphoneOn;
+        Log.d("app print", "appSwitch before if: " + isSpeakerphoneOn);
+
+
+        if (isSpeakerphoneOn) {
             // AppWidget speaker image on
             remoteViews.setImageViewResource(R.id.imageButton, R.drawable.autospeakeron);
+            //SharedPreferences.Editor editor = sharedPreferences.edit();
+           //sharedPreferences.edit().putBoolean("switchKey", isSpeakerphoneOn).apply();
+            //isAppWidgetOn = true;
+
+
+
+            Log.d("app print", "appSwitch true: " + isSpeakerphoneOn);
             // Set speakerphone on
             audioManager.setSpeakerphoneOn(isSpeakerphoneOn);
         }
-        else
-        {
+        else if (!isSpeakerphoneOn) {
+
             // AppWidget speaker image on
             remoteViews.setImageViewResource(R.id.imageButton, R.drawable.autospeakeroff);
+            Log.d("app print", "appSwitch false: " + isSpeakerphoneOn);
+
+
             // Set speakerphone Off
             audioManager.setSpeakerphoneOn(isSpeakerphoneOn);
         }
         ComponentName componentName = new ComponentName(context, PrototypeWidget.class);
         AppWidgetManager.getInstance(context).updateAppWidget(componentName, remoteViews);
         isSpeakerphoneOn = !isSpeakerphoneOn;
-        // Commit changes to SharedPreference
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-        editor.putBoolean("", isSpeakerphoneOn);
-        editor.commit();
-    }
-}
+        //sharedPreferences.edit().putBoolean("switchKey", isAppWidgetOn).apply();
 
+        // Commit changes to SharedPreference
+        sharedPreferences.edit().putBoolean("", isSpeakerphoneOn).apply();
+        sharedPreferences.edit().putBoolean("switchKey", isAppWidgetOn).apply();
+
+    }
+
+
+
+
+}
