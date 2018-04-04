6e17795503e22c302041f25d0b3cf597156e3509
GianlucaParilli <gluka47@hotmail.com>
2018-04-04 14:42:17 -0400

    added fav list

--- app/src/main/java/gluka/autospeakerphone/FavoriteList.java
+++ app/src/main/java/gluka/autospeakerphone/FavoriteList.java
@@ -1,13 +1,37 @@
 package gluka.autospeakerphone;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.Toast;
 
 public class FavoriteList extends AppCompatActivity {
-
+    private static final int PICK_CONTACT = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_favorite_list);
+
+        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+        startActivityForResult(intent, PICK_CONTACT);
+
     }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent intent)
+    {
+        if (requestCode == PICK_CONTACT)
+        {
+            Cursor cursor =  managedQuery(intent.getData(), null, null, null, null);
+            cursor.moveToNext();
+            String contactId = cursor.getString(cursor.getColumnIndex(ContactsContract.Contacts._ID));
+            String  name = cursor.getString(cursor.getColumnIndexOrThrow(ContactsContract.Contacts.DISPLAY_NAME));
+
+            Toast.makeText(this, "Contect LIST  =  "+name, Toast.LENGTH_LONG).show();
+        }
+    }
+
+
+
 }
